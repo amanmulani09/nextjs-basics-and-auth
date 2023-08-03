60a0e9cd2ea6c7d804014be82143616096d3206e
amanmulani09 <mulaniaman0504@gmail.com>
2023-08-04 00:14:06 +0530

    dynamic routes

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -2,7 +2,7 @@ const UserProfile = ({params}:any)=>{
     return(
         <>
         <h2>UserProfile</h2>
-        <h4>User Profile  <span className="p-2 ml-2 bg-orange-500 rounded text-black">{params.id}</span></h4>
+        <h4>User Profile <span className="p-2 ml-2 bg-orange-500 rounded text-black">{params.id}</span></h4>
         </>
     )
 }
