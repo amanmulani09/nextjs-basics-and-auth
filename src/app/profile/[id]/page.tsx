d83e0f502ebc5a27a2c6f99d8144adcad402f1dd
amanmulani09 <mulaniaman0504@gmail.com>
2023-08-04 00:13:42 +0530

    worked on profile page

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,10 @@
+const UserProfile = ({params}:any)=>{
+    return(
+        <>
+        <h2>UserProfile</h2>
+        <h4>User Profile  <span className="p-2 ml-2 bg-orange-500 rounded text-black">{params.id}</span></h4>
+        </>
+    )
+}
+
+export default UserProfile;
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+const ProfilePage = ()=>{
+
+    return(
+        <>
+        <h2>Profile</h2>
+        <h4>Profile Page</h4>
+        </>
+    )
+}
+
+export default ProfilePage;
